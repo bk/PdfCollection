4eb6fc05f1b7c30146437c977c258a09e9bde70c
Baldur Kristinsson <baldur.kristinsson@gmail.com>
2016-02-10 11:15:52 +0000

    Use meta search functionality; also less verbose output

--- scripts/pdfcoll_search.pl
+++ scripts/pdfcoll_search.pl
@@ -10,35 +10,60 @@ binmode STDIN, ":encoding(UTF-8)";
 binmode STDOUT, ":encoding(UTF-8)";
 
 my $format = 'normal';
-my $query = shift or die usage();
-if ($query eq '-json') {
+my $fts_query = shift or die usage();
+if ($fts_query eq '-json') {
     $format = 'json';
-    $query = shift or die usage();
-} elsif ($query eq '-yaml') {
+    $fts_query = shift or die usage();
+} elsif ($fts_query eq '-yaml') {
     $format = 'yaml';
-    $query = shift or die usage();
-} elsif ($query =~ /^-/ && $query ne '-normal') {
-    die "ERROR: Unrecognized format specified: '$query'\n";
+    $fts_query = shift or die usage();
+} elsif ($fts_query eq '-normal') {
+    $fts_query = shift or die usage();
+} elsif ($fts_query =~ /^-/) {
+    die "ERROR: Unrecognized format specified: '$fts_query'\n";
 }
+my @meta_queries = @ARGV;
+
 
 my $fts = new PdfCollection::SQLiteFTS;
 my $mo = new PdfCollection::Meta;
 
 my %found = ();
 
-my $res = $fts->search($query);
+my $res = $fts->search(
+    $fts_query,
+    search_meta=>1,
+    meta_queries=>\@meta_queries);
 
 foreach my $row (@$res) {
     # keys: offsets snippet file_name page_id folder_sha1
     my $sha1 = $row->{folder_sha1};
     unless ($found{$sha1}) {
-        $found{$sha1} = $mo->read_meta($sha1);
+        my $meta = $mo->read_meta($sha1);
+        my @mk = keys %$meta;
+        foreach my $k (@mk) {
+            delete $meta->{$k} if $k =~ /^(pdfinfo|isbn_|now_ts)/;
+        }
+        $found{$sha1} = $meta;
         $found{$sha1}->{HITCOUNT} = 0;
         $found{$sha1}->{HITS} = [];
     }
     $found{$sha1}->{HITCOUNT}++;
-    my $display = {snippet=>$row->{snippet}};
-    $display->{page} = int($1) if $row->{file_name} =~ /page_(\d+)/;
+    my $display;
+    if ($row->{type} eq 'meta') {
+        # we want entries with meta matches first
+        $found{$sha1}->{HITCOUNT} += 10000;
+        $display = {
+            author  =>$row->{author},
+            title   =>$row->{title},
+            summary => $row->{summary},
+            page    => 0,
+        };
+    }
+    else {
+        $display = {snippet=>$row->{snippet}};
+        $display->{page} = int($1) if $row->{file_name} =~ /page_(\d+)/;
+    }
     push @{$found{$sha1}->{HITS}}, $display;
 }
 
@@ -83,16 +108,30 @@ sub to_res {
 }
 
 sub usage {
-    return qq[Usage: $0 [-json|-yaml|-normal] 'query expression'
+    return qq[Usage: $0 [-json|-yaml|-normal] 'fts-query' [meta-queries]
 
   Output formats:
+
     -json: Output as JSON
     -yaml: Output as YAML
     -normal: Output as semi-structured plaintext - the default
 
-  Query expression:
+  FTS-query expression:
+
     Use the syntax for sqlite FTS, with quote-grouping, uppercase
     boolean operators OR and AND, as well as NEAR. If you use a minus
     (meaning "not"), do not start the query with it.
-];
-}
+
+  Meta-queries:
+
+    These are string patterns for LIKE queries into meta data (they will be
+    surrounded by '%'s automatically). They will be ORed together. If none is
+    specified, the FTS query expression will be used as a meta query string
+    also (which will not work very well unless you are only searching for a
+    single term).
+
+  Ordering:
+    Results are ordered by number of hits. Each hit in the meta info counts
+    as 10,000 ordinary hits, so these are always placed at the top of the
+    reults.
+]; }
